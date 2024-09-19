31608758d93af22444a5e0d45ed4532a69b903d1
iamayoubhadria <iamayoubhadria@gmail.com>
2024-09-19 14:55:39 +0100

    Update NavBar.jsx

--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     <nav className='bg=white shadow-md'>
       <div className='container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center'>
         <div className='text-lg font-bold'>
-          <Link to="/">e-SHOP</Link>
+          <Link to="/">BEHWAYJOO</Link>
         </div>
         <div className='relative flex-1 mx-4'>
           <form action="">

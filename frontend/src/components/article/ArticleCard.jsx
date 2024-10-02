6323c0f45ff99f7443520402a0a0cfa036380bcb
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2024-10-02 20:46:36 +0100

    add document title and icon fix hide and show psw in forms change some colors

--- frontend/src/components/article/ArticleCard.jsx
+++ frontend/src/components/article/ArticleCard.jsx
@@ -4,30 +4,30 @@ import { Calendar, Tag, MessageCircle, User } from "lucide-react";
 
 import { Link } from "react-router-dom";
 
-function ArticleCard({ article, tags, categories  }) {
+function ArticleCard({ article, tags, categories }) {
   const content = article.contents?.[0] || {};
 
   return (
     <>
-     <div className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105">
+      <div className="bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105">
         <div className="p-6">
-          <Link
-            to={`/article/${article.id}/`}
-            className="text-2xl font-semibold mb-2 text-gray-800"
-          >
-            {content.title || "Untitled"}
+          <Link to={`/article/${article.id}/`}>
+            <p className="text-2xl font-semibold mb-2 text-gray-800">
+              {content.title || "Untitled"}
+            </p>
+
+            <p className="text-gray-600 mb-4 line-clamp-3">
+              {content.body || "No content available"}
+            </p>
+            <div className="flex items-center text-sm text-gray-500 mb-4">
+              <User size={16} className="mr-2" />
+              <span>{article.author || "Unknown author"}</span>
+            </div>
+            <div className="flex items-center text-sm text-gray-500 mb-4">
+              <Calendar size={16} className="mr-2" />
+              <span>{new Date(article.created_at).toLocaleDateString()}</span>
+            </div>
           </Link>
-          <p className="text-gray-600 mb-4 line-clamp-3">
-            {content.body || "No content available"}
-          </p>
-          <div className="flex items-center text-sm text-gray-500 mb-4">
-            <User size={16} className="mr-2" />
-            <span>{article.author || "Unknown author"}</span>
-          </div>
-          <div className="flex items-center text-sm text-gray-500 mb-4">
-            <Calendar size={16} className="mr-2" />
-            <span>{new Date(article.created_at).toLocaleDateString()}</span>
-          </div>
           <div className="flex flex-wrap gap-2 mb-4">
             {article.tags?.map((tagId) => (
               <span
@@ -49,8 +49,7 @@ function ArticleCard({ article, tags, categories  }) {
             </span>
           </div>
         </div>
-      </div> 
-          
+      </div>
     </>
   );
 }
